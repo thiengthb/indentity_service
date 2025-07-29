a83da0ac4deb98a08d4140a7f20a26a4f67be508
thiengthb <tranngocthien628@gmail.com>
2025-07-29 14:06:16 +0700

    chore: initialize project

---
+++ indentity_backend/src/main/java/com/thientnse/indentity/IndentityApplication.java
@@ -0,0 +1,13 @@
+package com.thientnse.indentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndentityApplication.class, args);
+	}
+
+}
